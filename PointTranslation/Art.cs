b7b8b0aef846a630afc7bcb054741ad0ccd6b5a8
Fowzi Shakkour <FOWZIS@amdocs.com>
2018-04-16 15:18:05 +0300

    Add project files.

---
+++ PointTranslation/Art.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PointTranslation
+{
+    static class Art
+    {
+        public static Texture2D Pixel { get; private set; }
+
+        public static void Load(GraphicsDevice graphicsDevice, ContentManager content)
+        {
+            Pixel = new Texture2D(graphicsDevice, 1, 1, false, SurfaceFormat.Color);
+            Pixel.SetData<Color>(new[] { Color.White });
+            //Pixel.SetData(new[] { Color.White });
+        }
+    }
+}
---
+++ PointTranslation/Extention.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+
+namespace PointTranslation
+{
+    public static class Extention
+    {
+        public static void DrawPixel(this SpriteBatch spriteBatch, Vector2 point, Color color, float thickness = 1f)
+        {
+
+            spriteBatch.Draw(Art.Pixel, point, null, color, 0f,Vector2.Zero, new Vector2(thickness), SpriteEffects.None, 0);
+        }
+
+        public static void DrawLine(this SpriteBatch spriteBatch, Vector2 point1, Vector2 point2, Color color, float thickness = 1f)
+        {
+            var distance = Vector2.Distance(point1, point2);
+            var angle = (float)Math.Atan2(point2.Y - point1.Y, point2.X - point1.X);
+            DrawLine(spriteBatch, point1, distance, angle, color, thickness);
+        }
+
+        public static void DrawLine(this SpriteBatch spriteBatch, Vector2 point, float length, float angle, Color color, float thickness = 1f)
+        {
+            var origin = new Vector2(0f, 0.5f);
+            var scale = new Vector2(length, thickness);
+            spriteBatch.Draw(Art.Pixel, point, null, color, angle, origin, scale, SpriteEffects.None, 0);
+        }
+    }
+}
